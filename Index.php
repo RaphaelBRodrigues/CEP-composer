7838f31df10b2aac230060784fd416c73b7a3aa6
Raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2019-12-18 14:50:12 -0300

    requisição sem class funcionando

--- Index.php
+++ Index.php
@@ -1,3 +1,23 @@
 <?php
-phpinfo();
+
+
+
+
+
+
+
+
+function getAddress(string $cep):array{
+  $url = "https://viacep.com.br/ws/";
+
+  $get = file_get_contents($url.$cep.'/json');
+return (array) json_decode($get);
+}
+
+$cep = '05880-250';
+$cep = str_replace("-",'',$cep);
+
+$req = getAddress($cep);
+echo "Seu Cep é:".$cep;
+echo "\n <br>Sua Rua é:".$req['logradouro'];
  ?>
